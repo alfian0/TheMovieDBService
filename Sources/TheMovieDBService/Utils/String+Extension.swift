e5acb31b68279fc3aeca6301475a0f00683217aa
alfiansyah <alfian.official.mail@gmail.com>
2024-07-23 21:39:36 +0700

    use module

--- Sources/TheMovieDBService/Utils/String+Extension.swift
+++ Sources/TheMovieDBService/Utils/String+Extension.swift
@@ -8,9 +8,7 @@
 import Foundation
 
 public extension String {
-  func localized(languageCode: String?) -> String {
-    let path = Bundle.theMovieDBService.path(forResource: languageCode, ofType: "lproj")
-    let bundle = Bundle(path: path!)
-    return NSLocalizedString(self, tableName: "Localizable", bundle: bundle!, value: self, comment: self)
+  func localized() -> String {
+    return NSLocalizedString(self, tableName: "Localizable", bundle: .module, value: self, comment: self)
   }
 }
